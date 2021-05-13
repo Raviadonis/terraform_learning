d98774d30fc422a63c93d78350e66181c508abf6
Ravichandran <rav.adonis@gmail.com>
2021-05-13 12:26:48 +0530

    Update main.tf

--- aws/basic/instance/main.tf
+++ aws/basic/instance/main.tf
@@ -1,2 +1,8 @@
-resource
-adding new line 
+# Create an EC2 instance
+resource "aws_instance" "web" {
+  ami           = var.image_id
+  instance_type = var.machine_type
+    tags = {
+      Name = var.instance_name
+  }
+}
