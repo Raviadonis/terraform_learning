41cfaae7a5d8ff86c871558886d730f393e28080
Ravichandran <rav.adonis@gmail.com>
2021-12-13 07:27:23 +0530

    Create variables.tf

---
+++ openstack/security_group/variables.tf
@@ -0,0 +1,23 @@
+variable "ingress_rules" {
+  default = ""
+}
+
+variable "egress_rules" {
+  default = ""
+}
+
+variable "ingress_sg" {
+  default = ""
+}
+
+variable "egress_sg" {
+  default = ""
+}
+
+variable "sg_id" {
+  default = ""
+}
+
+variable "src_sg_id" {
+  default = ""
+}
